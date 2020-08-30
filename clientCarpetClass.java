c2484256881abb2da99337632624873152f68c53
jocelynstrmec <jocelynstrmec@localhost>
2020-08-30 15:44:31 -0700

    Merge branch 'master' of https://github.com/Strmec01/CPTS233.git

--- clientCarpetClass.java
+++ clientCarpetClass.java
@@ -1,23 +1,28 @@
 package AssigmentCarpets;
+/** Jocelyn Strmec
+ * Aug 30,2020
+ * Assignment 1 Carpet Calculator
+ */
 import java.util.*;
 
 public class clientCarpetClass {	
-	double c;
+	double cost;
 	public static void main(String [] args) {
 		
 		RoomCarpet rc = new RoomCarpet();
 		RoomDimension rd = new RoomDimension();
+		
 		try (Scanner input = new Scanner(System.in)) {
 			System.out.println("What is the width of the room you are carpeting?");
 			rd.width = input.nextDouble();
 			System.out.println("What is the length of the room you are carpeting?");
 			rd.length =input.nextDouble();
 			System.out.println("What is the desired cost per square foor of carpet?");
-			c = input.nextDouble();
-			rc.CarpetCost = c;
+			cost = input.nextDouble();
+			rc.carpetCost = cost;
 		}
-		input.close;
-		System.out.println("The total cost of the carpet is" + rc.getTotalCost(c,rd.getArea()));
+		
+		System.out.println("The total cost of the carpet is $" + rc.getTotalCost(cost,rd.getArea()));
 
 	}
 
