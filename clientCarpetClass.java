8285e2baf703fb77baa0fb61f5697b4249c882f9
jocelynstrmec <jocelynstrmec@localhost>
2020-08-30 16:00:07 -0700

    Changed Comments Again

--- clientCarpetClass.java
+++ clientCarpetClass.java
@@ -2,6 +2,8 @@ package AssigmentCarpets;
 /** Jocelyn Strmec
  * Aug 30,2020
  * Assignment 1 Carpet Calculator
+ * CPTS 233: MicroAssignment 1
+ * Github URL: https://github.com/Strmec01/CPTS233.git
  */
 import java.util.*;
 
