3d3b1b68dde996df7dda92389825fd62bb76fe66
jocelynstrmec <jocelynstrmec@localhost>
2020-08-30 15:09:53 -0700

    Nearing End of Program Requirements

--- RoomCarpet.java
+++ RoomCarpet.java
@@ -2,16 +2,20 @@ package AssigmentCarpets;
 
 public class RoomCarpet {
 	public double area;
-	public double costpersquareft;
+	public double carpetCost
+	public double totalCost;
 	
 	public void Carpet() {
 		RoomDimension Obj = new RoomDimension();
 		area = Obj.getArea();
 	}
 	public double getTotalCost(double cost,double area) {
-		double totalCost=cost*area;
+		totalCost=cost*area;
 		return totalCost;
 	}
-
+	
+	public String toString() {
+		return "RoomCarpet [area=" + area + ", Carpet Cost=" + carpetCost + ", total Cost=" + totalCost + "]";
+	}
 
 }
--- clientCarpetClass.java
+++ clientCarpetClass.java
@@ -2,20 +2,23 @@ package AssigmentCarpets;
 import java.util.*;
 
 public class clientCarpetClass {	
+	double w,l;
 	public static void main(String [] args) {
 		
 		RoomCarpet rc = new RoomCarpet();
 		RoomDimension rd = new RoomDimension();
 		try (Scanner input = new Scanner(System.in)) {
 			System.out.println("What is the width of the room you are carpeting?");
-			rd.width = input.nextDouble();
+			w = input.nextDouble();
+			rd.width = w;
 			System.out.println("What is the length of the room you are carpeting?");
-			rd.length =input.nextDouble();
+			l =input.nextDouble();
+			rd.length = l;
 			System.out.println("What is the desired cost per square foor of carpet?");
 			rc.CarpetCost = input.nextDouble();
 		}
 		
-		Systen.out.println("The total cost of the carpet is" + )
+		System.out.println("The total cost of the carpet is" + rc.getTotalCost);
 
 	}
 
